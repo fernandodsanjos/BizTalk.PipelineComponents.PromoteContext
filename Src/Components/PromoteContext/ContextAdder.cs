84cf7be3fd82542a78bee5f53641714871b85097
ferra71@hotmail.com <https://ibizsolutions.visualstudio.com/DefaultCollection/APL/_git/Shared.Schemas.EHM>
2017-08-17 14:41:25 +0200

    Added possiblity to set property type as suffix to property name

--- Src/Components/PromoteContext/ContextAdder.cs
+++ Src/Components/PromoteContext/ContextAdder.cs
@@ -16,6 +16,7 @@ using System.Threading;
 using System.Collections;
 using System.Collections.Specialized;
 using System.ComponentModel.Design;
+using System.Text.RegularExpressions;
 
 namespace BizTalk.PipelineComponents
 {
@@ -100,12 +101,41 @@ namespace BizTalk.PipelineComponents
 
         public IBaseMessage Execute(IPipelineContext pContext, IBaseMessage pInMsg)
         {
+           
 
             IEnumerator e = Properties.GetEnumerator();
             while (e.MoveNext())
             {
                 ContextValue v = (ContextValue)e.Current;
-                    pInMsg.Context.Promote(v.Key, v.Namespace, v.Value);
+
+                TypeCode type = TypeCode.String;
+                string property = v.Key;
+
+                Match match = Regex.Match(v.Key, @"@(\d)$");
+                if (match.Success == true)
+                {
+                    /*
+                    Byte 6
+                    DateTime 16
+                    Decimal 15
+                    Double 14
+                    Int16 7
+                    Int32 9
+                    sbyte 5
+                    Single 13
+                    String 18
+                    UInt16 8
+                    UInt32 10
+                    */
+
+
+                    type = (TypeCode)Enum.Parse(typeof(TypeCode), match.Groups[1].Value);
+                    property = property.Substring(0, property.Length - match.Value.Length);
+                }
+
+                var value = Convert.ChangeType(v.Value, type);
+
+                pInMsg.Context.Promote(property, v.Namespace, value);
 
             }
             return pInMsg;
